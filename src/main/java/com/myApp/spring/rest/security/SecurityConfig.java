dbd0641262e41394864ac0ef0f1d0c79768bc921
Georgi Radev <39059150+georgiradev@users.noreply.github.com>
2020-02-29 11:21:15 +0200

    Update SecurityConfig.java

--- src/main/java/com/myApp/spring/rest/security/SecurityConfig.java
+++ src/main/java/com/myApp/spring/rest/security/SecurityConfig.java
@@ -35,7 +35,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.PUT, "/api/employees").hasAnyRole("MANAGER", "ADMIN")
                 .antMatchers(HttpMethod.PUT, "/api/employees/**").hasAnyRole("MANAGER", "ADMIN")
                 .antMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN")
-                .antMatchers(HttpMethod.GET, "/actuator/**").hasRole("ADMIN")
+                .antMatchers( "/actuator/**").hasRole("ADMIN")
                 .and()
                 .httpBasic()
                 .and()
